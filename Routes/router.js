15739e59d42ca49f977d1c93f6ea712f1c04fd56
vikas <vikaspatil212222@gmail.com>
2023-03-15 15:59:23 +0530

    login

--- Routes/router.js
+++ Routes/router.js
@@ -79,6 +79,17 @@ router.post("/login",async (req,res)=>{
 
 
 })
+router.post('/checkUser',async(req,res)=>{
+    const { email } = req.body;
+    const existingUser = await UserInfo.findOne({email:email});
+    if(existingUser){
+        return res.status(200).json({message:"userExists"});
+    }else{
+        return res.status(200).json({message:"noUserExists"});
+
+    }
+
+})
 
 
 
