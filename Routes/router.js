174d1d1e1ca01421d29c4639f26ed32ff99d20b1
vikas <vikaspatil212222@gmail.com>
2023-04-28 18:23:54 +0530

    jwt

--- Routes/router.js
+++ Routes/router.js
@@ -10,106 +10,113 @@ import { UserInfo, WomenProductInfo, MenProductInfo, KidsProductInfo } from '../
 const router = express.Router()
 
 
-const ensureToken = (req,res,next) =>{
+const ensureToken = (req, res, next) => {
     const bHeader = req.headers["authorization"];
-    if(typeof bHeader != 'undefined'){
+    if (typeof bHeader != 'undefined') {
         const bToken = (bHeader.split(' '))[1];
         req.token = bToken;
 
         next();
-    }else{
-        res.status(403).json({msg:"Invalid Token"});
+    } else {
+        res.status(403).json({ msg: "Invalid Token" });
     }
 }
 
 // verifying the token
-const verifyToken = (token)=>{
-    return Jwt.verify(token,process.env.TOKEN_SECRET_KEY,(err,data)=>{
-        if(err){
-            return {err:err.message};
-        }else{
+const verifyToken = (token) => {
+    return Jwt.verify(token, process.env.TOKEN_SECRET_KEY, (err, data) => {
+        if (err) {
+            return { err: err.message };
+        } else {
             return data;
         }
     })
 }
 
 
-router.post("/register", async(req, res) => {
+router.post("/register", async (req, res) => {
 
-    const { email, password} = req.body;
+    const { email, password } = req.body;
 
     try {
-        const existingUser = await UserInfo.findOne({email:email});
-        if(existingUser){
-            return res.status(200).json({error:"Looks like you already have an account. Log in!"});
+        const existingUser = await UserInfo.findOne({ email: email });
+        if (existingUser) {
+            return res.status(200).json({ error: "Looks like you already have an account. Log in!" });
         }
 
-        const hashPassword = await bcrypt.hash(password,10);
+        const hashPassword = await bcrypt.hash(password, 10);
 
         const user = await UserInfo.create({
-            email:email,
-        password:hashPassword
+            email: email,
+            password: hashPassword
         })
-        const token = Jwt.sign({email:user.email,id:user._id},process.env.TOKEN_SECRET_KEY,{expiresIn:'1d'});
-        res.status(200).json({token:token})
-        
+        const token = Jwt.sign({ email: user.email, id: user._id }, process.env.TOKEN_SECRET_KEY, { expiresIn: '1d' });
+        res.status(200).json({ token: token })
+
     } catch (error) {
         console.log(error.message);
-        
+
     }
 })
 
-router.post("/login",async (req,res)=>{
-    const {email , password} = req.body;
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
 
-    const existingUser = await UserInfo.findOne({email:email});
-        if(!existingUser){
-            return res.status(200).json({message:"user doesnot exists "});
-        }
-
-    
-    const matchPassword = await bcrypt.compare(password,existingUser.password);
-
-    if(!matchPassword){
-        return res.json({message:"Wrong password"});
+    const existingUser = await UserInfo.findOne({ email: email });
+    if (!existingUser) {
+        return res.status(200).json({ message: "user doesnot exists " });
     }
-    const token = Jwt.sign({email:existingUser.email,id:existingUser._id},process.env.TOKEN_SECRET_KEY,{expiresIn:'1d'});
-    res.status(200).json({token:token})
+
+
+    const matchPassword = await bcrypt.compare(password, existingUser.password);
+
+    if (!matchPassword) {
+        return res.json({ message: "Wrong password" });
+    }
+    const token = Jwt.sign({ email: existingUser.email, id: existingUser._id }, process.env.TOKEN_SECRET_KEY, { expiresIn: '1d' });
+    res.status(200).json({ token: token })
 
 
 
 })
-router.post('/checkUser',async(req,res)=>{
+router.post('/checkUser', async (req, res) => {
     const { email } = req.body;
-    const existingUser = await UserInfo.findOne({email:email});
-    if(existingUser){
-        return res.status(200).json({user:"userExists"});
-    }else{
-        return res.status(200).json({error:"noUserExists"});
+    const existingUser = await UserInfo.findOne({ email: email });
+    if (existingUser) {
+        return res.status(200).json({ user: "userExists" });
+    } else {
+        return res.status(200).json({ error: "noUserExists" });
 
     }
 
 })
 
-router.post('/matchPassword', async(req,res)=>{
-    const {email,password} = req.body;
-    const existingUser = await UserInfo.findOne({email:email});
-    const matchPassword = await bcrypt.compare(password,existingUser.password);
-    if(!matchPassword){
-        return res.status(200).json('bad')
+router.post('/matchPassword', ensureToken, async (req, res) => {
+    const { email, password } = req.body;
+    const existingUser = await UserInfo.findOne({ email: email });
+    if (verifyToken(req.token)) {
+
+        const matchPassword = await bcrypt.compare(password, existingUser.password);
+        if (!matchPassword) {
+            res.status(200).json('bad')
+        } else {
+
+            res.status(200).json("good")
+        }
+    } else {
+        res.status(403).json("Invalid Token")
     }
 
-    return res.status(200).json("good")
 
 })
 
-router.post('/deleteAccount',ensureToken, async(req,res)=>{
-    const {email} = req.body;
-    if(verifyToken(req.token)){
-        await UserInfo.deleteOne({email:email});
+router.post('/deleteAccount', ensureToken, async (req, res) => {
+    const { email } = req.body;
+    if (verifyToken(req.token)) {
+        await UserInfo.deleteOne({ email: email });
         res.status(200).json("deleted")
 
-    }else{
+    } else {
         res.status(403).json('Invalid Token')
 
     }
@@ -118,53 +125,53 @@ router.post('/deleteAccount',ensureToken, async(req,res)=>{
 
 })
 
-router.post('/changePassword', ensureToken,async(req,res)=>{
-    const {email,password} = req.body;
-    const hashPassword = await bcrypt.hash(password,10);
-    if(verifyToken(req.token)){
+router.post('/changePassword', ensureToken, async (req, res) => {
+    const { email, password } = req.body;
+    const hashPassword = await bcrypt.hash(password, 10);
+    if (verifyToken(req.token)) {
 
-        await UserInfo.updateOne({email:email},{password:hashPassword});
+        await UserInfo.updateOne({ email: email }, { password: hashPassword });
         res.status(200).json("good")
-    }else{
-res.status(403).json('Invalid Token')
+    } else {
+        res.status(403).json('Invalid Token')
     }
 
 
 
 })
 
-router.post('/address/crud/:action',ensureToken,async(req,res)=>{
+router.post('/address/crud/:action', ensureToken, async (req, res) => {
     try {
-        const {email,index} =  req.body
+        const { email, index } = req.body
         const data = req.body.address;
         const action = req.params.action;
 
 
-        if(verifyToken(req.token)){
+        if (verifyToken(req.token)) {
 
-            var userData = await UserInfo.findOne({email:email});
-            if(action==='add'){
-                
-                await UserInfo.updateOne({email:email},{address:[...userData.address,data]});
+            var userData = await UserInfo.findOne({ email: email });
+            if (action === 'add') {
+
+                await UserInfo.updateOne({ email: email }, { address: [...userData.address, data] });
                 res.status(200).json('address added')
-    
-            }else if(action==='remove'){
-                var filArr = userData.address.filter((each,i)=>{
-                    if(index!==i){
+
+            } else if (action === 'remove') {
+                var filArr = userData.address.filter((each, i) => {
+                    if (index !== i) {
                         return each;
                     }
-                        
+
                 })
-    
-                await UserInfo.updateOne({email:email},{address:filArr});
+
+                await UserInfo.updateOne({ email: email }, { address: filArr });
                 res.status(200).json('address removes')
-    
+
             }
-        }else{
+        } else {
             res.status(403).json('Invalid Token')
         }
-        
-        
+
+
     } catch (error) {
         res.status(200).json(error)
     }
@@ -173,58 +180,58 @@ router.post('/address/crud/:action',ensureToken,async(req,res)=>{
 })
 
 
-router.get('/stock/:gender/:category',async(req,res)=>{
+router.get('/stock/:gender/:category', async (req, res) => {
     const category = req.params.category;
     const gender = req.params.gender;
-    if(gender==='men'){
+    if (gender === 'men') {
         var data = await MenProductInfo.find();
 
-    }else if(gender=='women'){
+    } else if (gender == 'women') {
 
         data = await WomenProductInfo.find();
-    }else{
+    } else {
         data = await KidsProductInfo.find();
     }
     const filArr = data.filter((each) => {
-        if(category==='All'){
+        if (category === 'All') {
             return each;
-        }else if (category === each.category) {
+        } else if (category === each.category) {
             return each;
         }
     })
     res.status(200).json(filArr)
 });
 
-router.get('/product/:gender/:category/:productId',async(req,res)=>{
+router.get('/product/:gender/:category/:productId', async (req, res) => {
     const category = req.params.category;
     const gender = req.params.gender;
     const productId = req.params.productId;
-    if(gender==='men'){
+    if (gender === 'men') {
         var data = await MenProductInfo.find();
 
-    }else if(gender=='women'){
+    } else if (gender == 'women') {
 
         data = await WomenProductInfo.find();
-    }else{
+    } else {
         data = await KidsProductInfo.find();
     }
     const filArr = data.filter((each) => {
-         if (productId === each.productId) {
+        if (productId === each.productId) {
             return each;
         }
     })
     res.status(200).json(filArr)
 })
 
-router.get('/address/:email',ensureToken,async(req,res)=>{
+router.get('/address/:email', ensureToken, async (req, res) => {
     try {
-        
-        const email = req.params.email;
-        if(verifyToken(req.token)){
 
-            var data = await UserInfo.findOne({email:email});
+        const email = req.params.email;
+        if (verifyToken(req.token)) {
+
+            var data = await UserInfo.findOne({ email: email });
             res.status(200).json(data.address)
-        }else{
+        } else {
             res.status(403).send('Invalid Token')
         }
     } catch (error) {
