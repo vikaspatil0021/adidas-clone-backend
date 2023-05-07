ff46667b67c043a1613b081b9c7c37cc0ae2f3eb
vikas <vikaspatil212222@gmail.com>
2023-05-07 09:55:38 +0530

    search filter

--- Routes/router.js
+++ Routes/router.js
@@ -229,90 +229,92 @@ router.post('/wishlist/crud/:action/:email', ensureToken, async (req, res) => {
 
 
 router.post('/search', async (req, res) => {
-        var query = req.body.query.toLowerCase();
-        var queryArr = query.split(' ');
+    var query = req.body.query.toLowerCase();
+    var queryArr = query.split(' ');
 
 
-        if (query.includes('women')) {
-            var data = await WomenProductInfo.find();
-            // var data = [...women].map((each) => {
-            //     return { ...each, url: '/women/All/' + each.productId }
-            // });
-        } else if (query.includes('men')) {
-            data = await MenProductInfo.find();
-            // data = [...men].map((each) => {
-            //     return { ...each, url: '/men/All/' + each.productId }
-            // });
-        } else if (query.includes('kids')) {
-            data = await KidsProductInfo.find();
-            // data = [...kids].map((each) => {
-            //     return { ...each, url: '/kids/All/' + each.productId }
-            // });
-        } else {
-
-            const men = await MenProductInfo.find();
-            const women = await WomenProductInfo.find();
-            const kids = await KidsProductInfo.find();
-
-           
-            // [...women].forEach((each) => {
-            //     return { ...each, url: '/women/All/' + each.productId }
-            // }),
-            // [...kids].forEach((each) => {
-            //     return { ...each, url: '/kids/All/' + each.productId }
-            // })]
-            data = [...men.map((each) => {
-                let e = each.toObject()
-                return { ...e, url: '/men/All/' + e.productId }
-            }),
-                 ...women,
-                  ...kids];
-
-        }
-        var cateAndTag = []
-        data.filter((item) => {
-            let one = false
-            queryArr.forEach((eachQry) => {
-                if (item.category.toLowerCase().includes(eachQry) && eachQry != '' && eachQry.length > 2) {
-                    cateAndTag.push(item);
-                } else if (item.tag.includes(eachQry) && eachQry != '' && eachQry.length > 2) {
-                    cateAndTag.push(item);
-                }
-                one = true
-            })
-
-
+    if (query.includes('women')) {
+        let women = await WomenProductInfo.find();
+        var data = women.map((each) => {
+            let e = each.toObject()
+            return { ...e, url: '/women/All/' + e.productId }
         })
-
-        var nameFilter = []
-        data.filter((item) => {
-            let one = false
-            queryArr.forEach((eachQry) => {
-
-                if (item.name.toLowerCase().includes(eachQry) && eachQry != '' && eachQry.length > 2 && one == false) {
-                    nameFilter.push(item);
-                    one = true
-                }
-            })
-
+    } else if (query.includes('men')) {
+        let men = await MenProductInfo.find();
+        data = men.map((each) => {
+            let e = each.toObject()
+            return { ...e, url: '/men/All/' + e.productId }
         })
+    } else if (query.includes('kids')) {
+        let kids = await KidsProductInfo.find();
+        data = kids.map((each) => {
+            let e = each.toObject()
+            return { ...e, url: '/kids/All/' + e.productId }
+        })
+    } else {
 
+        const men = await MenProductInfo.find();
+        const women = await WomenProductInfo.find();
+        const kids = await KidsProductInfo.find();
 
+        data = [...men.map((each) => {
+            let e = each.toObject()
+            return { ...e, url: '/men/All/' + e.productId }
+        }),
+        ...women.map((each) => {
+            let e = each.toObject()
+            return { ...e, url: '/women/All/' + e.productId }
+        }),
+        ...kids.map((each) => {
+            let e = each.toObject()
+            return { ...e, url: '/kids/All/' + e.productId }
+        })];
 
-        if (query == 'men' || query === 'women' || query == 'kids') {
-            var final = data;
-
-        } else {
-            if (cateAndTag.length === 0) {
-                final = nameFilter;
-
-            } else {
-
-                final = cateAndTag;
+    }
+    var cateAndTag = []
+    data.filter((item) => {
+        let one = false
+        queryArr.forEach((eachQry) => {
+            if (item.category.toLowerCase().includes(eachQry) && eachQry != '' && eachQry.length > 2) {
+                cateAndTag.push(item);
+            } else if (item.tag.includes(eachQry) && eachQry != '' && eachQry.length > 2) {
+                cateAndTag.push(item);
             }
+            one = true
+        })
 
+
+    })
+
+    var nameFilter = []
+    data.filter((item) => {
+        let one = false
+        queryArr.forEach((eachQry) => {
+
+            if (item.name.toLowerCase().includes(eachQry) && eachQry != '' && eachQry.length > 2 && one == false) {
+                nameFilter.push(item);
+                one = true
+            }
+        })
+
+    })
+
+
+
+    if (query == 'men' || query === 'women' || query == 'kids') {
+        var final = data;
+
+    } else {
+        if (cateAndTag.length === 0) {
+            final = nameFilter;
+
+        } else {
+
+            final = cateAndTag;
         }
-        res.status(200).json(final);
+
+    }
+    res.status(200).json(final);
 
 })
 
